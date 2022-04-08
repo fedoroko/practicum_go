189f40e9134615d765a0c87329c1aec42c759d88
Alexander Fedorchenko <fedoroko@Alexanders-MacBook-Air.local>
2022-04-09 00:44:19 +0200

    increment_4 increase report

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -4,8 +4,8 @@ import "github.com/fedoroko/practicum_go/internal/agent"
 
 func main() {
 	agent.Run(
-		agent.WithPollInterval(2),
-		agent.WithReportInterval(10),
+		agent.WithPollInterval(1),
+		agent.WithReportInterval(2),
 		agent.WithShutdownInterval(500),
 		agent.WithContentType(agent.ContentTypeJSON),
 	)
